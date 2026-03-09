f060a5fdea9f41d196eb1ac57d83eb001d6a9d7b
pohlnorman <npohl82@gmail.com>
2026-03-09 17:07:26 -0300

    se elimina rutas y controladores sin usar

--- src/routes/cita.route.js
+++ src/routes/cita.route.js
@@ -1,20 +1,13 @@
 import { Router } from "express";
 import {
-  listarCitas,
   formularioCrearCita,
   crearCita,
-  formularioEditarCita,
-  actualizarCita,
-  eliminarCita,
 } from "../controllers/cita.controller.js";
 
 const router = Router();
 
-router.get("/", listarCitas);
+
 router.get("/crear", formularioCrearCita);
 router.post("/crear", crearCita);
-router.get("/editar/:id", formularioEditarCita);
-router.post("/editar/:id", actualizarCita);
-router.get("/eliminar/:id", eliminarCita);
 
 export default router;
